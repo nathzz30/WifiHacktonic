7522e3c0925846fc46c245d566ba9da765fa5536
nathalyramirez <nathzzpersia@gmail.com>
2019-03-06 17:34:18 -0400

    without error

--- app/src/main/java/com/example/wifihacktonic/wifihacktonic/MainActivity.java
+++ app/src/main/java/com/example/wifihacktonic/wifihacktonic/MainActivity.java
@@ -1,26 +1,19 @@
 package com.example.wifihacktonic.wifihacktonic;
 
-import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.List;
 
