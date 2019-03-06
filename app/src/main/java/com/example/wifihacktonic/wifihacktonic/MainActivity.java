6616bd188e4dd1a195d68a689fca84605817873c
nathalyramirez <nathzzpersia@gmail.com>
2019-03-06 17:32:36 -0400

    first commit2

---
+++ app/src/main/java/com/example/wifihacktonic/wifihacktonic/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.wifihacktonic.wifihacktonic;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private WifiManager wifiManager;
+    private ListView listView;
+    private TextView tv2;
+    private Button buttonScan;
+    private int size=0;
+    private List <ScanResult> results;
+    private ArrayList <String> arrayList = new ArrayList<>();
+    private ArrayList <String> mac = new ArrayList<>();
+    private ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonScan= findViewById(R.id.scanBtn);
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                scanwifi();
+            }
+        });
+
+        listView = (ListView) findViewById(R.id.wifilist);
+        tv2 = (TextView) findViewById(R.id.tv2);
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        if (!wifiManager.isWifiEnabled())
+        {
+            Toast.makeText(this, "Wifi is disabled ... You need to enable it", Toast.LENGTH_LONG).show();
+            wifiManager.setWifiEnabled(true);
+        }
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener
+        ((parent, view, position, id) ->
+        {
+               tv2.setText("La MAC Address de la red " + listView.getItemAtPosition(position) + "es: " + mac.get(position));
+        });
+        //scanwifi();
+        //pa q sirva el commit2
+    }
+
+    private void scanwifi ()
+    {
+        arrayList.clear();
+        registerReceiver(wifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        wifiManager.startScan();
+        Toast.makeText(this, "Scaning Wifi ...",Toast.LENGTH_SHORT).show();
+    }
+
+    BroadcastReceiver wifiReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            results = wifiManager.getScanResults();
+            unregisterReceiver(this);
+
+            for (ScanResult scanResult : results)
+            {
+                arrayList.add(scanResult.SSID);
+                mac.add(scanResult.BSSID);
+                adapter.notifyDataSetChanged();
+            }
+        }
+    };
+
+
+}
